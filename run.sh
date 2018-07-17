e7a7dbbc1e4f1cc010e029dfb5363591efa1bd9b
Keito120607 <34155063+Keito120607@users.noreply.github.com>
2018-07-17 14:54:21 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir bin || true
 mkdir result || true
-javac src/entry/Entry.java -sourcepath src -d bin
+javac src/entry/Entry.java -sourcepath src -d bin -encoding UTF-8 
 java -cp bin entry.Entry
